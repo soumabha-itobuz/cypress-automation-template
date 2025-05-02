7828072a314afbf8f77e9b6dbf8047c7ec05e3ee
soumabha-itobuz <soumabha@itobuz.com>
2025-05-02 23:12:26 +0530

    assertion added

--- cypress/e2e/spec/example-spec.cy.ts
+++ cypress/e2e/spec/example-spec.cy.ts
@@ -4,5 +4,6 @@ import { } from 'cypress';
 describe('Google Homepage', () => {
   it('should open Google in the browser', () => {
     cy.visit('https://www.google.com');
+    cy.contains('Google'); // Assert that "Google" is visible on the page
   });
 });

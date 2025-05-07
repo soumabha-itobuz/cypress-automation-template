fde4c075a43c0af88e54188667b3c4bc45316a0c
soumabha-itobuz <soumabha@itobuz.com>
2025-05-07 17:26:20 +0530

    report done for json

--- cypress.config.js
+++ cypress.config.js
@@ -8,10 +8,19 @@ module.exports = defineConfig({
     embeddedScreenshots: true,
     inlineAssets: true,
     saveAllAttempts: false,
+    reportDir: 'cypress/reports', // Explicitly set the report directory
   },
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
+      on('after:run', async () => {
+        const { generate } = require('cypress-mochawesome-reporter/generateReport');
+        await generate({
+          reportDir: config.reporterOptions.reportDir,
+          saveJson: false, // Ensure JSON is not saved
+          saveHtml: true,  // Explicitly save the report as an HTML file
+        });
+      });
       // implement node event listeners here
     },
   },
--- cypress/e2e/spec/example-spec.cy.ts
+++ cypress/e2e/spec/example-spec.cy.ts
@@ -4,7 +4,7 @@ import 'cypress-mochawesome-reporter/register';
 
 describe('Google Homepage', () => {
   it('should open Google in the browser', () => {
-    cy.visit('https://www.google.com');
+    cy.visit(Cypress.env('GOOGLE_URL')); // Use Cypress.env to read the URL
     cy.contains('Google'); // Assert that "Google" is visible on the page
   });
 });

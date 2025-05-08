be907d146373cf9d12df2dffaf4040a0fa73c128
soumabha-itobuz <soumabha@itobuz.com>
2025-05-08 16:09:28 +0530

    env setup done

--- cypress/e2e/spec/example-spec.cy.ts
+++ cypress/e2e/spec/example-spec.cy.ts
@@ -4,7 +4,7 @@ import 'cypress-mochawesome-reporter/register';
 
 describe('Google Homepage', () => {
   it('should open Google in the browser', () => {
-    cy.visit(Cypress.env('GOOGLE_URL')); // Use Cypress.env to read the URL
+    cy.visit(Cypress.env('host')); // Use Cypress.env to read the URL
     cy.contains('Google'); // Assert that "Google" is visible on the page
   });
 });
